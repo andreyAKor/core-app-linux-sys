7b795ad3284bebf977ae2e2a00d3988b7a9745d5
andrey.a.kor <info@andreykor.com>
2019-10-15 10:24:59 +0300

    Add report caller flag

--- config/logging.go
+++ config/logging.go
@@ -2,5 +2,6 @@ package config
 
 // Настройки логгирования
 type Logging struct {
-	Level string // Уровень логгирования, варианты: debug, info, warning, error, fatal и panic
+	Level          string // Уровень логгирования, варианты: debug, info, warning, error, fatal и panic
+	IsReportCaller bool   // Показывать информацию о в каком месте был вызван лог
 }
